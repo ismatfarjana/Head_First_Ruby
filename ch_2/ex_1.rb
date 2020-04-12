800574f0c0296926caf1f45410a31e38c6141acf
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-04-12 20:19:45 +1000

    adding rectangle class

--- ch_2/ex_1.rb
+++ ch_2/ex_1.rb
@@ -6,5 +6,22 @@
   "<img src='#{src}'' width='#{width}'' height='#{height}'/>"
  end
 
+ #creating class
+
+ class Rectangle
+    attr_accessor :width,:height
+    def initialize(width, height)
+      @width = width
+      @height = height
+    end
+    def area
+      "Ther area is #{width*height}"
+    end
+
+ end
+
  p paragraph("i love you")
  p image("puppy.jpg", 800, 600)
+
+ reat = Rectangle.new(20,30)
+ p reat.area
