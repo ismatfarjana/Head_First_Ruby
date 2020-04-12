e8b5f26968498126e0318fdb3e5ccb361b51b131
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-04-12 20:38:02 +1000

    throwing an error

--- ch_2/ex_1.rb
+++ ch_2/ex_1.rb
@@ -9,19 +9,34 @@
  #creating class
 
  class Rectangle
-    attr_accessor :width,:height
+    attr_reader :width,:height
     def initialize(width, height)
       @width = width
       @height = height
     end
-    def area
-      "Ther area is #{width*height}"
+    
+
+    def width=(value)
+         value < 0 ? (raise "Value cannt be negative" ): @width = value 
+         
     end
+
+    def height=(value)
+      if value < 0 
+        raise "Value cannt be negative"
+      end
+      @height = value  
+  end
+
+  def area
+    "Ther area is #{width*height}"
+  end
 
  end
 
  p paragraph("i love you")
  p image("puppy.jpg", 800, 600)
 
- reat = Rectangle.new(20,30)
+ reat = Rectangle.new(-2,30)
  p reat.area
+ p reat.width= -2
